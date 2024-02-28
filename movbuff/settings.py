89168e3936b37cd1796a2e7c82c4a0fc5a3bde76
Pranshu <priyampranshu@gmail.com>
2024-02-28 18:56:30 +0530

    added requirements.txt

--- movbuff/settings.py
+++ movbuff/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 if DEBUG:
     ALLOWED_HOSTS = ['*']
